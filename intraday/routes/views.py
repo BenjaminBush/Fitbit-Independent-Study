ed1ff2dbbfb53c1f0c9c63eb8967dd2d9a0225c1
Daniel Borstelmann <dborstelmann@gmail.com>
2016-03-27 00:36:20 -0500

    fuck me

--- intraday/routes/views.py
+++ intraday/routes/views.py
@@ -10,10 +10,8 @@ def home():
 
 @intraday.route('/auth_redirect')
 def auth_redirect():
-    print('hello')
 
     cur = intraday.db.cursor()
-    print('cur')
 
     result = requests.post('https://api.fitbit.com/oauth2/token', data={
         'client_id': '227FD3',
@@ -22,18 +20,15 @@ def auth_redirect():
         'redirect_uri': 'https://intraday.herokuapp.com/auth_redirect',
         'code': request.args.get('code')
     })
-    print('result:')
     result = result.json()
-    print('result-json:')
 
     insert_user_query = """
             INSERT INTO users (
                     id,
-                    device_name,
                     access_token,
                     refresh_token
                 )
-            VALUES (%s, %s, %s, %s)"""
+            VALUES (%s, %s, %s)"""
     cur.execute(insert_user_query, (
         result['user_id'],
         result['access_token'],
