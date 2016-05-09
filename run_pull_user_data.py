d52ac6cbfed02d5e60727134d525428e5719bed6
Daniel Borstelmann <dborstelmann@gmail.com>
2016-05-08 22:06:49 -0500

    I AM A GOD

--- run_pull_user_data.py
+++ run_pull_user_data.py
@@ -1,28 +1,7 @@
-import requests, psycopg2, base64, fitbit, datetime
+import requests, psycopg2, base64, fitbit, datetime, json
 #Connect to the databases using psycopg2
 conn = psycopg2.connect("dbname='dc7qf7cii79rie' password='9yuoOs84Nr7k-r3H7ioZxVzPoV' user='qhazfngzupdgdj' host='ec2-54-83-22-48.compute-1.amazonaws.com' port='5432'")
 conn.autocommit = True
-
-# def revoke_access():
-#     clientId = '227FD3'
-#     clientSecret = '5543280369ea955f96decf9e635c29f9'
-#     cur = conn.cursor()
-#     cur.execute("""
-#         SELECT id, access_token, refresh_token FROM users
-#     """)
-#     users = cur.fetchall()
-#
-#     access_token = users[1][1]
-#
-#     result = requests.post('https://api.fitbit.com/oauth2/revoke', data={
-#         'token': access_token
-#     }, headers={
-#         'Authorization': b'Basic ' + base64.b64encode(('227FD3:5543280369ea955f96decf9e635c29f9').encode('utf8')),
-#         'Content-Type': 'application/x-www-form-urlencoded'
-#     })
-#
-#     print result
-
 
 def get_user():
     #Store the id and secret for future reference
@@ -35,79 +14,86 @@ def get_user():
     """)
     users = cur.fetchall()
 
+    for user in users:
+        user_id = user[0]
+        user_access_token = user[1]
+        user_refresh_token = user[2]
 
-    user_id = users[1][0]
-    user_access_token = users[1][1]
-    user_refresh_token = users[1][2]
-
-    #Use the api to get the resources
-    authd_client = fitbit.Fitbit(clientId, clientSecret,
-                                 access_token=user_access_token, refresh_token=user_refresh_token)
+        #Use the api to get the resources
+        authd_client = fitbit.Fitbit(clientId, clientSecret,
+                                     access_token=user_access_token, refresh_token=user_refresh_token)
 
 
-    #Refresh the token here just so we don't run into any problems
-    result = requests.post('https://api.fitbit.com/oauth2/token', data={
-        'grant_type': 'refresh_token',
-        'refresh_token': user_refresh_token
-    }, headers={
-        'Authorization': b'Basic ' + base64.b64encode(('227FD3:5543280369ea955f96decf9e635c29f9').encode('utf8')),
-        'Content-Type': 'application/x-www-form-urlencoded'
-    })
-    import pdb; pdb.set_trace()
+        #Refresh the token here just so we don't run into any problems
+        result = requests.post('https://api.fitbit.com/oauth2/token', data={
+            'grant_type': 'refresh_token',
+            'refresh_token': user_refresh_token
+        }, headers={
+            'Authorization': b'Basic ' + base64.b64encode(('227FD3:5543280369ea955f96decf9e635c29f9').encode('utf8')),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        })
 
-    if result['success']:
-        user_access_token = result['access_token']
-        user_refresh_token = result['refresh_token']
+        json_response = json.loads(result.content)
 
-    print result
+        print json_response
 
-    cur.execute("""
-            UPDATE users SET access_token = %s, refresh_token = %s WHERE id = %s
-        """, user_access_token, user_refresh_token, user_id)
+        if 'errors' in json_response:
+            print('failed miserably')
+        else:
+            user_access_token = json_response['access_token']
+            user_refresh_token = json_response['refresh_token']
 
-    #Make the request
-    URLBASE = URLBASE = "%s/%s/user" % (fitbit.Fitbit.API_ENDPOINT, fitbit.Fitbit.API_VERSION)
-    resource = "activities"
-    date = datetime.datetime.today().strftime("%Y-%m-%d")
-    user_id = user_id
-    data = None
-    url = URLBASE + "/%s/%s.json" % (user_id, resource)
-    data = fitbit.Fitbit._COLLECTION_RESOURCE(authd_client, resource, date, user_id, data)
+            cur.execute("""
+                    UPDATE users SET access_token = %s, refresh_token = %s WHERE id = %s
+                """, (user_access_token, user_refresh_token, user_id))
 
-    #Store the important data in local variables
-    distance = data['summary']['distances'][1]['distance']
-    floors = data['summary']['floors']
-    steps = data['summary']['steps']
-    calsout = data['summary']['caloriesOut']
 
-    try:
-        restinghr = data['summary']['restingHeartRate']
-    except KeyError:
-        restinghr = 0
+            #Make the request
+            URLBASE = URLBASE = "%s/%s/user" % (fitbit.Fitbit.API_ENDPOINT, fitbit.Fitbit.API_VERSION)
+            resource = "activities"
+            date = datetime.datetime.today().strftime("%Y-%m-%d")
+            user_id = user_id
+            data = None
+            url = URLBASE + "/%s/%s.json" % (user_id, resource)
+            data = fitbit.Fitbit._COLLECTION_RESOURCE(authd_client, resource, date, user_id, data)
 
-    #Create the query
-    insert_user_query = """
-            INSERT INTO loggables (
-                    id,
-                    distance,
-                    floors,
-                    steps,
-                    restinghr,
-                    calsout,
-                    day
-                )
-            VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+            #Store the important data in local variables
+            distance = data['summary']['distances'][1]['distance']
+            floors = data['summary']['floors']
+            steps = data['summary']['steps']
+            calsout = data['summary']['caloriesOut']
 
-    #Insert into the table
-    cur.execute(insert_user_query, (
-        user_id,
-        distance,
-        floors,
-        steps,
-        restinghr,
-        calsout,
-        date
-    ))
+            try:
+                restinghr = data['summary']['restingHeartRate']
+            except KeyError:
+                restinghr = 0
+
+            #Create the query
+            insert_user_query = """
+                    INSERT INTO loggables (
+                            id,
+                            distance,
+                            floors,
+                            steps,
+                            restinghr,
+                            calsout,
+                            day
+                        )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+
+            #Insert into the table
+            cur.execute(insert_user_query, (
+                user_id,
+                distance,
+                floors,
+                steps,
+                restinghr,
+                calsout,
+                date
+            ))
+
+    cur.close()
+
 
 #Call the function!
 get_user()
